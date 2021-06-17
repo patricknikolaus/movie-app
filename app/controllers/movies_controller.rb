02043eb6b67d22a5afc0c8c215b6a20374882f87
Patrick <pdn6991@gmail.com>
2021-06-17 12:27:32 -0400

    Added routes to movie controller to show one and all movies.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,13 @@
 class MoviesController < ApplicationController
+  def get_one_movie
+    one_movie = Movie.first
+    render json: {movie: one_movie}
+  end
+
+  def get_all_movies
+    all_movies = Movie.all
+    render json: {movies: all_movies}
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
 
 get "/one_actor", controller: "actors", action: "get_one_actor"
+get "/one_movie", controller: "movies", action: "get_one_movie"
+get "/all_movies", controller: "movies", action: "get_all_movies"
 end
