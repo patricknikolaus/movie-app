1aea0431ca09529574c5ec804478eb3f566e1a8e
Patrick <pdn6991@gmail.com>
2021-06-17 12:22:21 -0400

    Added movie model and movies controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
-actor = Actor.new({first_name: "Robert", last_name: "Downey, JR", known_for: "Iron Man"})
-actor.save
+movie = Movie.new(title: "Avengers: Infinity War", year: 2018, plot: "Super heros deal with a dick.")
+movie.save
