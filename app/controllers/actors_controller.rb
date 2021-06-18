298c5ab5cb7a4108d4f88d0e456b194c52682c13
Patrick <pdn6991@gmail.com>
2021-06-18 14:50:36 -0400

    Added route and method to print actor with query param

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,6 +1,7 @@
 class ActorsController < ApplicationController
-  def get_one_actor
-    find_actor = Actor.find_by(id: 6)
-    render json: {actor: find_actor}
+  def show
+    the_id = params[:id]
+    actor = Actor.find_by(id: the_id)
+    render json: actor.as_json
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-
-get "/one_actor", controller: "actors", action: "get_one_actor"
 get "/one_movie", controller: "movies", action: "get_one_movie"
 get "/all_movies", controller: "movies", action: "get_all_movies"
 post "/pick_an_actor", controller: "movies", action: "choose_an_actor"
+get "/actor", controller: "actors", action: "show"
 end
