a57399e65f4ce46f84e8ed3a226487c1d2b9a73a
Patrick <pdn6991@gmail.com>
2021-06-18 14:52:46 -0400

    Added route to print actor with url segement params

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 get "/one_movie", controller: "movies", action: "get_one_movie"
 get "/all_movies", controller: "movies", action: "get_all_movies"
 post "/pick_an_actor", controller: "movies", action: "choose_an_actor"
+
 get "/actor", controller: "actors", action: "show"
+get "/actor/:id", controller: "actors", action: "show"
 end
